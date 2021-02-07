09ae4536f6422c684b970327343242f61e53e688
rainers <35530071+rainersyt@users.noreply.github.com>
2021-02-07 13:03:50 +0200

    Upload *

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DiscordRPC;
+
+namespace DRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Context = new DiscordRpcClient("795615344319528980"); // this id is for a minecraft rpc that I tried making a bit ago
+            Context.Initialize();
+
+            Context.UpdateState("for 2 years");
+            Context.UpdateLargeAsset("minecraft");
+
+            Button BTN = new Button();
+            BTN.Label = "OwO?~";
+            BTN.Url = "https://owowhatsthis.xyz/";
+
+            Context.SetPresence(RP);
+            
+            System.Threading.Thread.Sleep(-1);
+        }
+        private static void JoinRequested(object sender, DiscordRPC.Message.JoinRequestMessage args)
+        {
+            
+        }
+    }
+}
